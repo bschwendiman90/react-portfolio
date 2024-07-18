d287b37975d063b7e6ce668b14b2331825162176
Ben Schwendiman <bschwendiman90@gmail.com>
2024-07-18 12:14:05 -0600

    portfolio skeleton

---
+++ react-protfolio/src/components/Portfolio.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const projects = [
+    { title: 'Project 1', imgSrc: '/path/to/image1.jpg', appLink: 'http://deployed-app1.com', repoLink: 'http://github.com/repo1', description: 'This is a description of project 1' },
+    { title: 'Project 1', imgSrc: '/path/to/image1.jpg', appLink: 'http://deployed-app1.com', repoLink: 'http://github.com/repo1', description: 'This is a description of project 1' },
+    { title: 'Project 1', imgSrc: '/path/to/image1.jpg', appLink: 'http://deployed-app1.com', repoLink: 'http://github.com/repo1', description: 'This is a description of project 1' },
+    { title: 'Project 1', imgSrc: '/path/to/image1.jpg', appLink: 'http://deployed-app1.com', repoLink: 'http://github.com/repo1', description: 'This is a description of project 1' },
+    { title: 'Project 1', imgSrc: '/path/to/image1.jpg', appLink: 'http://deployed-app1.com', repoLink: 'http://github.com/repo1', description: 'This is a description of project 1' },
+    { title: 'Project 1', imgSrc: '/path/to/image1.jpg', appLink: 'http://deployed-app1.com', repoLink: 'http://github.com/repo1', description: 'This is a description of project 1' },
+    ];
+
+    const Portfolio = () => {
+        <section>
+            <h2>Portfolio</h2>
+            <div className='projects'>
+                {projects.map((project, index) => (
+                    <div key={index} className='project'>
+                        <img src={project.imgSrc} alt={project.title} />
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <a href={project.appLink}>Deployed</a>
+                        <a href={project.repoLink}>GitHub</a>
+                    </div>
+                ))}
+            </div>
+
+        </section>
+    };
+
+    export default Portfolio;
