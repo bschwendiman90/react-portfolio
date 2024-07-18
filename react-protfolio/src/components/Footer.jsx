2556d3c61cf93f8874b57671ba9b5280979bc639
Ben Schwendiman <bschwendiman90@gmail.com>
2024-07-18 12:00:21 -0600

    footer skeleton

---
+++ react-protfolio/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    <footer>
+        <a href='https://github.com/bschwendiman90' target='_blank' rel='noopener noreferrer'>GitHub</a>
+        <a href='https://www.linkedin.com/in/ben-schwendiman-1b72352b5//' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
+        <a href='https://stackoverflow.com/users/22772147/bschwendiman90' target='_blank' rel='noopener noreferrer'>Stack Overflow</a>
+    </footer>
+};
+
+export default Footer;
