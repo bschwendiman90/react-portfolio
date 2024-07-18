f6bc6d4a6d708884526103017a364368778aafb6
Ben Schwendiman <bschwendiman90@gmail.com>
2024-07-18 11:54:35 -0600

    header skeleton

---
+++ react-protfolio/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import react from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    <header>
+        <h1>
+            Ben Schwendiman
+        </h1>
+        <nav>
+            <NavLink to="/" activeClassName="active">About Me</NavLink>
+            <NavLink to="/portfolio" activeClassName="active">Portfolio</NavLink>
+            <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+            <a href='../assets/resume.pdf' download>Resume</a>
+        </nav>
+    </header>
+};
+
+    export default Header;
