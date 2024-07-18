35d8da6b827bf065f8b8671ddc953f168facd5c8
Ben Schwendiman <bschwendiman90@gmail.com>
2024-07-18 12:25:31 -0600

    about me skeleton

---
+++ react-protfolio/src/components/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+    <section>
+        <h2>About Me</h2>
+        <p>
+            I am a full stack web developer with a background in education. I have a passion for learning and creating. I am excited to be a part of the future of technology.
+        </p>
+    </section>
+};
+
+export default AboutMe;
